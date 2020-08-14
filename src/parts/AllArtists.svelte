ff79190e2c43377ae006d855ec003ead1d36ffe2
Daniel Narey <danielnarey@users.noreply.github.com>
2020-08-13 22:10:19 -0400

    test faunadb integration

---
+++ src/parts/AllArtists.svelte
@@ -0,0 +1,31 @@
+<script>
+  import { onMount } from 'svelte';
+  
+  let artists = [];
+
+  const getArtists = async () => {
+    artists = [];
+    const res = await fetch('/.netlify/functions/all-artists');
+    const json = await res.json();
+    console.log(json);
+    
+    return json.map((item) => ({
+      id: item.ref['@ref'].id,
+      name: item.data.name,
+    }));
+  };
+  
+  onMount(async () => {
+    artists = await getArtists();
+  });
+  
+</script>
+
+<h2>Response to the test query follows:</h2>
+<ul>
+  {#each artists as artist}
+    <li>
+      {artist.name} (ref: {artist.id})
+    </li>
+  {/each}
+</ul>
--- src/routes/User.svelte
+++ src/routes/User.svelte
@@ -1,4 +1,11 @@
+<script>
+  import AllArtists from '../parts/AllArtists.svelte';
+  
+</script>
+
 <template lang="pug">
   p This is the user page.
+  
+  AllArtists
 
 </template>
